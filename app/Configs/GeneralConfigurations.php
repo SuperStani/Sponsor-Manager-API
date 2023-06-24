1fa8c7774a573c135915790601d76ba51c15810f
Stanislav Zatic <superstani.dev@gmail.com>
2023-06-24 13:26:26 +0200

    add token

--- app/Configs/GeneralConfigurations.php
+++ app/Configs/GeneralConfigurations.php
@@ -8,5 +8,5 @@ interface GeneralConfigurations
 {
     public const APP_PREFIX = "SponsorAPI";
 
-    public const BOT_TOKEN = "";
+    public const BOT_TOKEN = "5961895426:AAHy5ctZXNXsbixVbb_Gvv1ts3aCpUrxCLI";
 }
