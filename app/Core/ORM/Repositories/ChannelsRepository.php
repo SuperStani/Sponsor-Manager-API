fc96a5e8cafb17c33d7dbf71cac863175fc4cecb
Stanislav Zatic <superstani.dev@gmail.com>
2023-07-09 13:30:43 +0200

    modify query of channels

--- app/Core/ORM/Repositories/ChannelsRepository.php
+++ app/Core/ORM/Repositories/ChannelsRepository.php
@@ -18,29 +18,28 @@ class ChannelsRepository
     public function getInviteUrlsByBotUsername(string $bot_username): array
     {
         $query = "
-            SELECT 
-                   channels.channel_id, 
-                   ciu.invite_url
-            FROM (
-                     SELECT c.channel_id, c.datetime
-                     FROM channels c
-                     WHERE c.users_range IS NOT NULL
-                       AND c.users_range <= c.earned_users
-                     UNION
-                     SELECT c1.channel_id, c1.datetime
-                     FROM channels c1
-                     WHERE c1.datetime_start < NOW()
-                       AND c1.datetime_stop > NOW()
-                     ORDER BY datetime -- Add an ORDER BY clause to specify the desired order
-                     LIMIT 5 -- Limit the results in the subquery
-                 ) channels
-            LEFT JOIN channels_invite_urls ciu
-            ON ciu.channel_id = channels.channel_id
-            WHERE ciu.bot_username = ?
-            ORDER by channels.datetime LIMIT 5
+        SELECT channels.channel_id,
+               channels.invite_url,
+               channels.bot_username
+        FROM (
+                 SELECT c.channel_id, c.invite_url, c.datetime, c.bot_username
+                 FROM channels c
+                 WHERE c.users_range IS NOT NULL
+                   AND c.users_range <= c.earned_users
+                 UNION
+                 SELECT c1.channel_id, c1.invite_url, c1.datetime, c1.bot_username
+                 FROM channels c1
+                 WHERE c1.datetime_start < NOW()
+                   AND c1.datetime_stop > NOW()
+                 ORDER BY datetime -- Add an ORDER BY clause to specify the desired order
+                 LIMIT 5 -- Limit the results in the subquery
+             ) channels
+        WHERE channels.bot_username = ?
+        ORDER by channels.datetime
+        LIMIT 5
         ";
         $res = $this->db->query($query, $bot_username);
-        if($res) {
+        if ($res) {
             $data = [];
             foreach ($res as $row) {
                 $data[] = ['channel_id' => $row['channel_id'], "invite_url" => $row['invite_url']];
