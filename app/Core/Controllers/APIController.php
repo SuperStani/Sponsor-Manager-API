a1762adef97dcfa4185e90238d3b4945de08b367
Stanislav Zatic <superstani.dev@gmail.com>
2023-09-23 19:09:47 +0200

    fix bug

--- app/Core/Controllers/APIController.php
+++ app/Core/Controllers/APIController.php
@@ -56,7 +56,7 @@ class APIController
                 $data = ["result" => true, "user_id" => $_GET['user_id'], "channels" => [], "is_subscribed_all" => true];
                 foreach ($channelsData as $channel) {
                     $is_subscribed = $this->channelsService->checkUserOnChannel($channel['channel_id'], $_GET['user_id']);
-                    $data['channels'] = [
+                    $data['channels'][] = [
                         'id' => $channel['channel_id'],
                         "is_subscribed" => $is_subscribed,
                         "invite_url" => $channel['invite_url']
